d8021b01ab7ec31382189f5c7382aea0fd63c14d
Jean Talar <67374151+jeanfwl@users.noreply.github.com>
2021-09-21 18:03:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+const startMatch = document.getElementById('start-match')
+const addEnergy = document.getElementById('add-energy');
+const removeEnergy = document.getElementById('remove-energy');
+const energySpan = document.getElementById('energy');
+const nextTurn = document.getElementById('next-turn');
+const turnSpan = document.getElementById('turn');
+const matchContent = document.querySelector('.match-content');
+
+let turn = 1;
+let energy = 3;
+const maxEnergy = 10;
+energySpan.innerText = energy;
+turnSpan.innerText = turn;
+
+startMatch.addEventListener('click', (e) => {
+    e.preventDefault();
+    startMatch.innerText = 'New match';
+    matchContent.classList.add('active');
+});
+
+addEnergy.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (energy + 1 > maxEnergy) return;
+    energy++;
+    energySpan.innerText = energy;
+});
+
+removeEnergy.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (energy <= 0) return;
+    energy--;
+    energySpan.innerText = energy;
+
+});
+
+nextTurn.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (energy + 2 > maxEnergy) {
+        energy = maxEnergy;
+    } else {
+        energy += 2;
+    }
+    energySpan.innerText = energy;
+    turn++;
+    turnSpan.innerText = turn;
+});
